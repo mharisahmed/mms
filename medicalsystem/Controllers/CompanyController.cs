6ff3f1ab38445498ed1eba7e583b851137d44f68
mharisahmed <m.harisahmed@hotmail.com>
2016-08-24 20:40:01 +0500

    Companies Form Created

---
+++ medicalsystem/Controllers/CompanyController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using constrng;
+
+namespace medicalsystem.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult Index()
+        {
+            var result = DataServices.Services.CompanyService.Instance.Companies();
+            return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tblCompay company)
+        {
+            DataServices.Services.CompanyService.Instance.AddCompany(company);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var getCompanyForEdit = DataServices.Services.CompanyService.Instance.EditCompany(id);
+            return View(getCompanyForEdit);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(tblCompay  comp)
+        {
+            DataServices.Services.CompanyService.Instance.UpdateCompany(comp);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            DataServices.Services.CompanyService.Instance.DeleteCompany(id);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ medicalsystem/DataServices/Services/CompanyService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using constrng;
+
+namespace medicalsystem.DataServices.Services
+{
+    public class CompanyService
+    {
+        private static CompanyService _service = null;
+
+        public static CompanyService Instance => _service ?? (_service = new CompanyService());
+
+        public List<tblCompay> Companies()
+        {
+
+            using (var _context = DataContextHelper.GetContext())
+            {
+                var result = _context.Fetch<tblCompay>("select * from tblCompay");
+
+                return result;
+
+            }
+
+        }
+
+        //string compname, string compaddress, string compnemail
+        public void AddCompany(tblCompay compobject) {
+            using (var _context = DataContextHelper.GetContext())
+            {
+              _context.Insert(compobject);
+    
+            }
+        }
+
+        //Get Recode for Update
+        public tblCompay EditCompany(int id)
+        {
+            using (var _context = DataContextHelper.GetContext())
+            {
+                var Company =_context.Single<tblCompay>("Select * From tblCompay where CompanyId=@0", id);
+                return Company;
+            }
+        }
+
+        // Update Record.
+        public void UpdateCompany(tblCompay comp)
+        {
+            using (var _context = DataContextHelper.GetContext())
+            {
+                _context.Update(comp);
+            }
+
+        }
+
+        // Delete Record.
+        public void DeleteCompany(int id)
+        {
+            using (var _context = DataContextHelper.GetContext())
+            {
+                _context.Delete<tblCompay>(id);
+            }
+
+        }
+
+    }
+}
