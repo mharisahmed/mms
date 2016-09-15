8c78726d18f89ef6250a5c6f0703d73a4e602d31
Munawarkhan6656 <munawarkhan6656@gmail.com>
2016-09-15 02:09:11 -0700

    Web Service Implemented

---
+++ medicalsystem/scripts/Custom.js
@@ -0,0 +1,61 @@
+﻿function CompanyNameButton() {
+    $(document).ready(function () {
+
+        $('#txtUserName').keyup(function () {
+            var userName = $(this).val();
+
+            if (userName.length >= 3) {
+                $.ajax({
+                    url: '/WebServices/RegistrationService.asmx/UserNameExists',
+                    method: 'post',
+                    data: { userName: userName },
+                    dataType: 'json',
+                    success: function (data) {
+
+                        if (data.UserNameInUse) {
+                            $('#btnDiv').hide('fast');
+                        }
+                        else {
+                            $('#btnDiv').show('fast');
+                        }
+                    },
+                    error: function (err) {
+                        alert("error" + err);
+                    }
+                });
+            }
+        });
+    });
+}
+function CompanyName() {
+    $(document).ready(function () {
+
+        $('#txtUserName').keyup(function () {
+            var userName = $(this).val();
+
+            if (userName.length >= 3) {
+                $.ajax({
+                    url: '/WebServices/RegistrationService.asmx/UserNameExists',
+                    method: 'post',
+                    data: { userName: userName },
+                    dataType: 'json',
+                    success: function (data) {
+                        var divElement = $('#divOutput');
+                        if (data.UserNameInUse) {
+                            divElement.text(data.UserName + ' already in use');
+                            divElement.css('color', 'red');
+                        }
+                        else {
+                            divElement.text(data.UserName + ' available')
+                            divElement.css('color', 'green');
+                        }
+                    },
+                    error: function (err) {
+                        alert("error" + err);
+                    }
+                });
+            }
+        });
+    });
+}
+
