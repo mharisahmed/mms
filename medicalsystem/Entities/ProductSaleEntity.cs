abeb7e5e89f07f5628b64915c50b5ea30e170a8a
munawarkhan <munawarkhan6656@gmail.com>
2016-12-05 00:44:24 +0500

    sale from

---
+++ medicalsystem/Entities/ProductSaleEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace medicalsystem.Entities
+{
+    public class ProductSaleEntity
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int Rate { get; set; }
+        public int SDId { get; set; }
+        public int SMId { get; set; }
+    }
+}
--- medicalsystem/Models/ProductRecieved.cs
+++ medicalsystem/Models/ProductRecieved.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using constrng;
+using medicalsystem.Entities;
 
 namespace medicalsystem.Models
 {
@@ -10,5 +11,12 @@ namespace medicalsystem.Models
     {
         public  tblProductReceivedMaster ModelMaster {get; set;}
         public tblProductReceivedDetail ModelDetail { get; set; }
+    }
+    public class ProductSale
+    {
+        public  List<tblSaleMaster> SaleMaster { get; set; }
+        public tblSaleMaster SaleMastert { get; set; }
+        public tblSaleDetail SaleDetail { get; set; }
+        public List<ProductSaleEntity> SaleDetails { get; set; }
     }
 }
